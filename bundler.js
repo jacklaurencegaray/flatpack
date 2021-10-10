81fe5a76a56f4cdab718f9da7cbb6c0184cc7339
jacklaurencegaray <jacklaurencegaray@users.noreply.github.com>
2021-10-10 17:17:50 +0200

    Created main assets

--- bundler.js
+++ bundler.js
@@ -2,14 +2,29 @@ const fs = require("fs")
 const babylon = require("babylon")
 const traverse = require("babel-traverse").default
 
+let ID = 0
+
 // Take path to a file and extract its dependencies
 function createAsset(filename) {
  const content = fs.readFileSync(filename, "utf-8")
 
  const ast = babylon.parse(content, { sourceType: "module" })
+
+ const dependencies = []
+
  traverse(ast, {
-  ImportDeclaration: ({ node }) => {},
+  ImportDeclaration: ({ node }) => {
+   dependencies.push(node.source.value)
+  },
  })
+
+ const id = ID++
+
+ return {
+  id,
+  filename,
+  dependencies,
+ }
 }
 
-createAsset("./src/entry.js")
+const mainAsset = createAsset("./src/entry.js")
