7912910ed5ef2524c7bd670f2188310fd0825f1b
jacklaurencegaray <jacklaurencegaray@users.noreply.github.com>
2021-10-20 15:29:09 +0200

    Added bundler functionality, finalized createGraph

--- bundler.js
+++ bundler.js
@@ -1,6 +1,8 @@
 const fs = require("fs")
 const babylon = require("babylon")
 const traverse = require("babel-traverse").default
+const path = require("path")
+const babel = require("babel-core")
 
 let ID = 0
 
@@ -19,12 +21,57 @@ function createAsset(filename) {
  })
 
  const id = ID++
+ const { code } = babel.transformFromAst(ast, null, {
+  presets: ["env"],
+ })
 
  return {
   id,
   filename,
   dependencies,
+  code,
  }
 }
 
-const mainAsset = createAsset("./src/entry.js")
+function createGraph(entry) {
+ const mainAsset = createAsset(entry)
+
+ const queue = [mainAsset]
+
+ for (const asset of queue) {
+  const dirname = path.dirname(asset.filename)
+  asset.mapping = {}
+
+  asset.dependencies.forEach((relativePath) => {
+   const absolutePath = path.join(dirname, relativePath)
+   const child = createAsset(absolutePath)
+   asset.mapping[relativePath] = child.id
+   queue.push(child)
+  })
+ }
+ return queue
+}
+
+function bundle(graph) {
+ let modules = ""
+
+ graph.forEach(
+  (mod) =>
+   (modules += `${mod.id}: [
+     function (require, module, exports) {
+        ${mod.code}
+     },
+    ],`)
+ )
+
+ const result = `
+        (function() {
+
+        })({${modules}})
+    `
+
+ return result
+}
+
+const graph = createGraph("./src/entry.js")
+const result = bundle(graph)
